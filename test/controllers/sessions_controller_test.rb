aa53abbcc365c6d35ff8bede58fc15fa18819a62
h-ueno2 <menbou1212@gmail.com>
2020-07-04 19:06:19 +0900

    8.2

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,15 +2,5 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
-  test "login with invald information" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" }}
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
-  end
-  
 
 end
