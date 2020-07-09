7305c36c72da25ee4246a4ecec5eaeab6f92cca1
h-ueno2 <menbou1212@gmail.com>
2020-07-09 19:50:46 +0900

    10.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,7 @@ class UsersController < ApplicationController
     # ログイン済ユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please lo in."
         redirect_to login_url
       end
